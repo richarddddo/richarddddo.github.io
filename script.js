95ee6aeaea655f9d37f01141c4016c75da7b8184
richarddddo <richy.dokr@gmail.com>
2023-12-13 16:41:43 +1100

    new page

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-// You can add JavaScript functionality if needed
-// For example, handling picture displays, animations, etc.
-// This file can be left empty if no JS functionality is required
+let cartCount = 0;
+
+function addToCart(productName) {
+  cartCount++;
+  updateCartCount(cartCount);
+}
+
+function updateCartCount(count) {
+  const cartLink = document.getElementById('cartLink');
+  cartLink.textContent = `Shopping Cart (${count})`;
+}
+
+function goToCart() {
+  // Redirect to cart page
+  window.location.href = 'cart.html';
+}
+
+function addToCart(productName, price) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const item = { name: productName, price: price };
+    cart.push(item);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    updateCartCount(cart.length);
+  }
+
+  function updateCartCount(count) {
+    const cartLink = document.getElementById('cartLink');
+    cartLink.textContent = `Shopping Cart (${count})`;
+  }
